ae281302bbef53184708d68e714d4d9f6ed1d9d4
Asha <ashalbenjamin@gmail.com>
2020-05-31 20:31:30 -0500

    added index and server files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const resourcesRouter = require('./route/resource-router');
+const projectRouter = require('./route/projects-router');
+const tasksRouter = require('./route/tasks-router');
+
+// const db = require('./data/db-config');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+
+server.get('/', (req, res) => {
+	res.send('it works');
+});
+
+module.exports = server;
