aa5cf780b4ffcc4000b38446ea40e91b508f74dd
Asha <ashalbenjamin@gmail.com>
2020-05-31 20:36:59 -0500

    knex file generated

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+module.exports = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './projects.db3',
+		},
+		useNullAsDefault: true,
+	},
+	migrations: {
+		directory: './data/migrations',
+	},
+	seeds: {
+		directory: './data/seeds',
+	},
+	pool: {
+		afreCreate: (conn, done) => {
+			conn.run('PRAGMA foreign_keys = ON', done);
+		},
+	},
+};
